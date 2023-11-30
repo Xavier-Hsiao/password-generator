f8144e3914ff139822a104d17e40d6e642c3af70
Xavier-Hsiao <tim88776@gmail.com>
2023-11-30 15:09:30 +0800

    Build: add basic get and post route

--- app.js
+++ app.js
@@ -8,7 +8,15 @@ app.engine(".hbs", engine({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
 app.set("views", "./views");
 
+// To parse request body object
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/", (req, res) => {
+  console.log(req.body);
   res.render("index");
 });
 
